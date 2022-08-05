d414ec9c489318800efd3869d1e81097ead72ddf
Hibagon1go <hibarayoichiro871@gmail.com>
2022-08-05 21:58:03 +0900

    change dir name chatapp->frontend

--- frontend/pages/room/[room_id].tsx
+++ frontend/pages/room/[room_id].tsx
@@ -99,13 +99,9 @@ const ChatRoom: NextPage = () => {
               className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
             >
               {msgs.map((msg, key) => {
-                const isMine = key % 2 === 0; // FIXME:const isMine = msg.name === hogehoge_name; // TODO: 自分のnameがわからん
-                const justifyEndOrStart = isMine
-                  ? "justify-end"
-                  : "justify-start";
                 return (
                   <div key={key} className="chat-message">
-                    <div className={`flex items-end ${justifyEndOrStart}`}>
+                    <div className={`flex items-end justify-start`}>
                       <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
                         <div>
                           <span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">
@@ -114,12 +110,12 @@ const ChatRoom: NextPage = () => {
                           </span>
                         </div>
                       </div>
+                      <a>{msg.name}</a>
                       <img
                         src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144"
                         alt="My profile"
                         className="w-6 h-6 rounded-full order-2"
                       />
-                      <a>{msg.name}</a>
                     </div>
                   </div>
                 );
